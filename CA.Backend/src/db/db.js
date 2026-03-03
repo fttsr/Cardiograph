5ba9fd8f30c3ce2e7b95e9e58fa11c451b81cd2c
fttsr <fetteser.niox@gmail.com>
2026-03-03 08:12:47 +0700

    Добавлен запрос для проверки корректного подключения к БД

--- CA.Backend/src/db/db.js
+++ CA.Backend/src/db/db.js
@@ -11,3 +11,16 @@ export const pool = new Pool({
   database: process.env.DB_NAME,
   port: 5432,
 });
+
+pool
+  .query(
+    `
+  SELECT
+    current_database() as db,
+    current_schema() as schema,
+    current_user as usr,
+    inet_server_addr() as host,
+    inet_server_port() as port
+`,
+  )
+  .then((r) => console.log("DB INFO:", r.rows[0]));
