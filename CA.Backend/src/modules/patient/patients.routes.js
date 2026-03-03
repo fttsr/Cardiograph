d75920ead100cf1fc2c231858af87f8f5c7fd422
fttsr <fetteser.niox@gmail.com>
2026-03-03 11:05:16 +0700

    Удалена старая папка после переименования

--- CA.Backend/src/modules/patient/patients.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as controller from "./patient.controller.js";
-
-const router = express.Router();
-
-router.get("/", controller.search);
-
-export default router;
--- CA.Backend/src/modules/report/report.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as controller from "./report.controller.js";
-
-const router = express.Router();
-
-router.post("/", controller.create);
-
-export default router;
