17b48c3824fd472e0defc67b1ed5a448235eed97
fttsr <fetteser.niox@gmail.com>
2026-03-03 10:32:45 +0700

    Добавлен маршрут для измерений в app.js

--- CA.Backend/src/app.js
+++ CA.Backend/src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import consultationRoutes from "./modules/consultation/consultation.routes.js";
 import authRoutes from "./modules/auth/auth.routes.js";
 import reportRoutes from "./modules/report/report.routes.js";
+import measurementRoutes from "./modules/measurement/measurement.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use("/consultation", consultationRoutes);
 app.use("/auth", authRoutes);
 app.use("/reports", reportRoutes);
+app.use("/measurement", measurementRoutes);
 
 export default app;
